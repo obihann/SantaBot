991691ac84bbaf1ac9636453c29567d71ac5eb0e
Jeff Hann <jeffhann@gmail.com>
2016-11-20 20:11:46 -0400

    updates to match new santaslist modulea

--- santaBot.py
+++ santaBot.py
@@ -1,16 +1,24 @@
 import yaml
-import SantasList
+import santaslist
 
 from errbot import BotPlugin, botcmd
+from pathlib import Path
+
+YML_FILE = 'people.yml'
 
 
 class SantaBot(BotPlugin):
     def __init__(self, *args, **kwargs):
-        data = open('people.yml', 'r')
-        SantasList.load_people(yaml.load(data)['people'])
+        if Path(YML_FILE).is_file():
+            data = open('people.yml', 'r')
+            santaslist.load_people(yaml.load(data)['people'])
 
         super(SantaBot, self).__init__(*args, **kwargs)
 
     @botcmd
+    def people(self, msg, args):
+        return santaslist.print_people()
+
+    @botcmd
     def pairs(self, msg, args):
-        return SantasList.list_matches()
+        return santaslist.print_pairs()
